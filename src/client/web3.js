79ff9aa4d5152a05fa1926b68173920b76454196
Strictly Scarce <strictly_scarce1@protonmail.com>
2020-05-05 12:57:33 +1000

    testnet
    Signed-off-by: Strictly Scarce <strictly_scarce1@protonmail.com>

--- src/client/web3.js
+++ src/client/web3.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 // import UNISWAP from '../artifacts/UniswapExchange.json'
 
 export const vetherAddr = () => {
-	return '0x830a6f5764b3e736f1f24427a6ea294926862195'
+	return '0xd447B74e5Ff9fAF98eB66dC59DC2C91DD47736dC'
 }
 
 export const vetherAbi = () => {
