ceb5b13443ab484e4cd3785d8c52f0155dff4456
Strictly Scarce <strictly_scarce1@protonmail.com>
2020-05-21 10:29:48 +1000

    update charts
    Signed-off-by: Strictly Scarce <strictly_scarce1@protonmail.com>

--- src/ui/pages/Trade.js
+++ src/ui/pages/Trade.js
@@ -22,7 +22,7 @@ const Trade = () => {
 			<H2>TRADE VETHER</H2><br />
 			<Subtitle>BUY AND SELL VETHER USING UNISWAP</Subtitle>
 			<br /><br />
-			<Click><a href='https://uniswap.exchange/swap/0x31Bb711de2e457066c6281f231fb473FC5c2afd3' rel="noopener noreferrer" title="Uniswap Link" target="_blank" style={{ color: "#D09800", fontSize: 12 }}>VIEW ON UNISWAP -></a></Click> <br/>
+			<Click><a href='https://v1.uniswap.exchange/swap?outputCurrency=0x31Bb711de2e457066c6281f231fb473FC5c2afd3' rel="noopener noreferrer" title="Uniswap Link" target="_blank" style={{ color: "#D09800", fontSize: 12 }}>VIEW ON UNISWAP -></a></Click> <br/>
 			<Click><a href='https://etherscan.io/address/0x506d07722744e4a390cd7506a2ba1a8157e63745' rel="noopener noreferrer" title="Etherscan Link" target="_blank" style={{ color: "#D09800", fontSize: 12 }}>VIEW ON ETHERSCAN -></a></Click>
 			<TradeTable></TradeTable>
 			{safari &&
