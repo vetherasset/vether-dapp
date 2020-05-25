96289bd10826df78a20a26694b1bc0a03ca6267e
0xBrian <51255357+0xBrian@users.noreply.github.com>
2020-05-25 19:40:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 3000);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join('build', 'index.html'));
+  });
+}
