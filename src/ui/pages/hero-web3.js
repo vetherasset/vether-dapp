8840023e2fef9549fe5b956ace063cdd2777b89e
yellowgh0st <burim.berisa@protonmail.com>
2020-06-25 12:39:05 +0200

    Hide price from hero card

--- src/ui/pages/hero-web3.js
+++ src/ui/pages/hero-web3.js
@@ -136,7 +136,7 @@ export const VetherTable = () => {
                         <Logo/>
                     </div>
                     <div style={{textAlign: 'center'}}>
-                        <Text size={32}>${(convertEthtoUSD(marketData.priceETH, 2))}</Text>
+                        <Text size={32} style={{ display: 'none'}}>${(convertEthtoUSD(marketData.priceETH, 2))}</Text>
                     </div>
                 </Col>
                 <Col xs={24} sm={16}>
