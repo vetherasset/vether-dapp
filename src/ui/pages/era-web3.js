6cead38cae47f7867534b6a4f8e7767133b833df
yellowgh0st <burim.berisa@protonmail.com>
2020-07-22 01:44:02 +0200

    Reload data on each render

--- src/ui/pages/era-web3.js
+++ src/ui/pages/era-web3.js
@@ -8,8 +8,7 @@ import { vetherAddr, vetherAbi, infuraAPI, getUniswapPriceEth } from '../../clie
 import { getETHPrice } from '../../client/market.js'
 import { convertFromWei, getSecondsToGo, prettify } from '../utils'
 
-import { Row, Col, Button, Progress } from 'antd'
-import { ReloadOutlined } from '@ant-design/icons';
+import { Row, Col, Progress } from 'antd'
 import { LabelGrey, Center, Text, Colour } from '../components'
 import { BurnCard } from '../ui'
 
@@ -31,24 +30,16 @@ export const EraTable = (props) => {
         if (!loaded) {
             loadBlockchainData()
             setLoaded(true)
-            // console.log('starting')
         }
-        // console.log('rendering again')
         // eslint-disable-next-line
     }, [loaded])
 
     const loadBlockchainData = async () => {
-        context.eraData ? await getEraData() : await loadEraData()
-        context.marketData ? await getMarketData() : await loadMarketData()
-    }
-
-    const getEraData = async () => {
-        setEraData(context.eraData)
-        setCounter(context.eraData.secondsToGo)
+        loadEraData()
+        loadMarketData()
     }
 
     const loadEraData = async () => {
-        // console.log('clicked')
         const web3 = new Web3(new Web3.providers.HttpProvider(infuraAPI()))
         const contract = new web3.eth.Contract(vetherAbi(), vetherAddr())
         const emission = 2048
@@ -72,9 +63,6 @@ export const EraTable = (props) => {
         context.setContext({ "eraData": eraData })
     }
 
-    const getMarketData = async () => {
-        setMarketData(context.marketData)
-    }
     const loadMarketData = async () => {
         const priceEtherUSD = await getETHPrice()
         const priceVetherEth = await getUniswapPriceEth()
@@ -96,26 +84,15 @@ export const EraTable = (props) => {
         }
     }
 
-    const refresh = async () => {
-        setEraData({
-            era: 1, day: eraData.day,
-            nextEra: eraData.nextEra, nextDay: eraData.nextDay,
-            emission: eraData.emission, nextEmission: eraData.nextEmission,
-            currentBurn: 0,
-            secondsToGo: 0
-        })
-        await loadEraData()
-    }
-
     useEffect(() => {
         counter >= 0 && setTimeout(() => updateTimer(), 1000);
         // eslint-disable-next-line
     }, [counter]);
 
     const updateTimer = () => {
-        var measuredTime = new Date(null);
+        let measuredTime = new Date(null);
         measuredTime.setSeconds(counter);
-        var MHSTime = measuredTime.toISOString().substr(11, 8);
+        let MHSTime = measuredTime.toISOString().substr(11, 8);
         setCounter(counter - 1)
         setTimer(MHSTime)
     }
@@ -129,33 +106,11 @@ export const EraTable = (props) => {
         backgroundColor: '#cc9300'
     }
 
-    const buttonStyles = {
-        color: Colour().gold,
-        backgroundColor: Colour().dgrey,
-        borderColor: Colour().dgrey,
-    }
-
     return (
-        <div>
-            {/* {!loaded &&
-                <div>
-                    <Row style={{ paddingTop: 100, paddingBottom: 100 }}>
-                        <LoadingOutlined style={{ marginLeft: 20, fontSize: 30 }} />
-                    </Row>
-                </div>
-            } */}
+        <>
             {loaded &&
                 <>
-                    <Row>
-                        <Col xs={24} sm={8}>
-                            {/* <Center> */}
-                            <Button id="emissionTimerRefreshBtn" nClick={refresh} style={buttonStyles}>
-                                <ReloadOutlined style={{ color: Colour().gold}} />
-                                REFRESH
-                            </Button>
-                            {/* </Center> */}
-                        </Col>
-
+                    <Row type="flex" justify="center">
                         <Col xs={24} sm={8}>
                             <div id="vetherEraTimer">
                                 <Text size={40} margin={"0px 0px"}>{timer}</Text>
@@ -169,13 +124,8 @@ export const EraTable = (props) => {
                                 </>
                             }
                         </Col>
-
-                        <Col xs={24} sm={8}>
-                        </Col>
                     </Row>
-                    <Row style={{ marginBottom: '3rem' }}>
-                        <Col xs={24} sm={6}>
-                        </Col>
+                    <Row type="flex" justify="center" style={{ marginBottom: '3rem' }}>
                         {small &&
                             <Col xs={24} sm={12}>
                                 <Center>
@@ -237,63 +187,14 @@ export const EraTable = (props) => {
                                 </Center>
                             </Col>
                         }
-                        <Col xs={24} sm={6}>
-                        </Col>
                     </Row>
-                    <Row style={{ marginTop: 20 }}>
-                        <Col xs={24} sm={8}>
-                        </Col>
+                    <Row type="flex" justify="center" style={{ marginTop: 20 }}>
                         {!small &&
                             <BurnCard marketData={marketData} eraData={eraData} />
                         }
-                        <Col xs={24} sm={8}>
-                        </Col>
                     </Row>
-
-                    {/* <Row>
-                        <Col xs={21} sm={11}>
-                            <Row>
-                                <Col xs={24}>
-                                    <LabelGrey>CURRENT ERA: </LabelGrey><br />
-                                    <Label>{eraData.era}</Label>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col xs={24}>
-                                    <LabelGrey>CURRENT DAY: </LabelGrey><br />
-                                    <Label>{eraData.day}</Label>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col xs={24}>
-                                    <LabelGrey>CURRENT EMISSION: </LabelGrey><br />
-                                    <Label>{eraData.emission}</Label><Text size={14}> VETH (per day)</Text>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col xs={21} sm={13}>
-                            <Row>
-                                <Col xs={24}>
-                                    <LabelGrey>NEW DAY: </LabelGrey><br />
-                                    <Label>{convertToTime(eraData.nextDay)} (local time)</Label>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col xs={24}>
-                                    <LabelGrey>HALVING DATE: </LabelGrey><br />
-                                    <Label>{convertToMonth(eraData.nextEra)}</Label>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col xs={24}>
-                                    <LabelGrey>NEXT EMISSION: </LabelGrey><br />
-                                    <Label>{prettify(eraData.nextEmission)}</Label><Text size={14}> VETH (per day)</Text>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row> */}
                 </>
             }
-        </div>
+        </>
     )
 }
