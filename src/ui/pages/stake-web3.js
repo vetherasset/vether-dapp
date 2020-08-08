1c5ac966b1ae62fe3ba85b25991abac0dc5b3f9e
Strictly-Scarce <strictly_scarce1@protonmail.com>
2020-08-08 17:25:01 +1000

    V2
    Signed-off-by: Strictly-Scarce <strictly_scarce1@protonmail.com>

--- src/ui/pages/stake-web3.js
+++ src/ui/pages/stake-web3.js
@@ -575,15 +575,10 @@ export const UpgradeTable = () => {
 			window.web3 = new Web3(window.ethereum);
 			var accounts = await window.web3.eth.getAccounts()
 			const address = await accounts[0]
-			// context.accountData ? await getAccountData() : 
 			await loadAccountData(address)
 			setConnected(true)
 			setLoading(false)
 		}
-	}
-
-	const getAccountData = async () => {
-		setAccount(context.accountData)
 	}
 
 	const loadAccountData = async (account) => {
