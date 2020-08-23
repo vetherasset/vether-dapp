af246ac31b1a9115eaa38a21a5d1e8ebb678f136
yellowgh0st <burim.berisa@protonmail.com>
2020-08-23 22:18:57 +0200

    Use WarningOutlined icon

--- src/ui/components/swapInterface.js
+++ src/ui/components/swapInterface.js
@@ -3,7 +3,7 @@ import { Context } from '../../context'
 import Web3 from 'web3'
 
 import { Row, Col, Input, Tooltip } from 'antd'
-import { SwapOutlined, QuestionCircleOutlined, LoadingOutlined, ExclamationCircleOutlined } from '@ant-design/icons'
+import { SwapOutlined, QuestionCircleOutlined, LoadingOutlined, WarningOutlined } from '@ant-design/icons'
 import { Label, Sublabel, Button, Colour, LabelGrey } from '../components'
 
 import { ETH, vetherAddr, vetherAbi, vetherPools2Addr, vetherPools2Abi, getEtherscanURL,
@@ -347,7 +347,7 @@ export const SwapInterface = () => {
                     { trade.slippageWarning &&
                         <>
                             <LabelGrey display={'block'} style={{ fontStyle: 'italic' }}>
-                                <ExclamationCircleOutlined style={{ marginBottom: '0' }}/>&nbsp;Due to trade size your price's affected by high slippage.
+                                <WarningOutlined style={{ marginBottom: '0' }}/>&nbsp;Due to trade size your price's affected by high slippage.
                             </LabelGrey>
                         </>
                     }
