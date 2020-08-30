2ce99ee85dfb2657c734bae0498457e131ed5dc4
yellowgh0st <burim.berisa@protonmail.com>
2020-08-30 02:29:47 +0200

    Implement unstakeExactAsymmetric to UI

--- src/ui/components/stakeDialog.js
+++ src/ui/components/stakeDialog.js
@@ -5,7 +5,7 @@ import { ETH, getEtherscanURL, getUniswapPriceEth, infuraAPI, vetherAbi, vetherA
     vetherPools2Addr, vetherPoolsAbi, vetherPoolsAddr } from "../../client/web3"
 import { convertFromWei, currency, getBN } from "../../common/utils"
 import { calcShare } from "../../common/clpLogic"
-import {Col, Slider, InputNumber, Input, Row, Select, Tooltip } from "antd"
+import { Col, Slider, InputNumber, Input, Row, Select, Tooltip } from "antd"
 import { LoadingOutlined, CheckCircleOutlined, QuestionCircleOutlined } from '@ant-design/icons'
 import {Button, Colour, Label, LabelGrey, Sublabel} from "../components"
 
@@ -54,10 +54,7 @@ export const StakeDialog = () => {
             let poolData = await poolContract.methods.poolData(ETH).call()
             let poolShare = calcShare(getBN(stakeData.stakeUnits), getBN(poolData.poolUnits), getBN(poolData.asset), getBN(poolData.vether))
 
-            // let vetherShare = await poolContract.methods.getStakerShareVether(account, ETH).call()
-            // let assetShare = await poolContract.methods.getStakerShareAsset(account, ETH).call()
             let memberROI = await poolContract.methods.getMemberROI(account, ETH).call()
-            console.log(memberROI)
 
             const accountData = {
                 'address': account,
@@ -226,7 +223,7 @@ const AddLiquidityTable = (props) => {
 
     return (
         <>
-            <h2 style={{ fontStyle: 'italic' }}>Select asset to pool.</h2>
+            <h2 style={{ fontStyle: 'italic' }}>Select asset to stake.</h2>
             <LabelGrey display={'block'} style={{ fontStyle: 'italic' }}>Select an asset you would like to provide. Vether pool is non-proportional. Unlike Uniswap, where you need to provide<br/>
                 an equal proportion of both assets, Vether pools allow you to provide liquidity in unequal proportions.</LabelGrey>
 
@@ -266,7 +263,7 @@ const AddLiquidityTable = (props) => {
                     ? <Button backgroundColor="transparent" onClick={stake}>ADD >></Button>
                     : <Button backgroundColor="transparent" disabled>ADD >></Button>
                 }
-                <Sublabel>ADD LIQUIDITY TO THE POOL</Sublabel>
+                <Sublabel>ADD LIQUIDITY TO POOL</Sublabel>
             </>
             }
 
@@ -358,97 +355,176 @@ const AddLiquidityTable = (props) => {
 const RemoveLiquidityTable = (props) => {
 
     const account = props.accountData
+    const poolContract = new window.web3.eth.Contract(vetherPools2Abi(), vetherPools2Addr())
 
-    const [burnTknFlag, setBurnTknFlag] = useState(null)
-    const [tknTx, setTknTx] = useState(null)
-    const [loaded2, setLoaded2] = useState(null)
-    const [unstakeAmount, setUnstakeAmount] = useState(0)
+    const assets = [
+        {
+            name: 'Vether',
+            symbol: '$VETH'
+        },
+        {
+            name: 'Ether',
+            symbol: 'Ξ'
+        }
+    ]
+
+    const [unstakeAssetSet, setUnstakeAssetSet] = useState(assets[0])
+    const [unstakeAssetAmount, setUnstakeAssetAmount] = useState(0)
+    const [unstakeAssetTx, setUnstakeAssetTx] = useState(undefined)
+    const [unstakeAssetFlag, setUnstakeAssetFlag] = useState(false)
+    const [unstakeAssetDone, setUnstakeAssetDone] = useState(false)
+
+    const [unstakeProportionAmount, setUnstakeProportionAmount] = useState(0)
+    const [unstakeProportionTx, setUnstakeProportionTx] = useState(undefined)
+    const [unstakeProportionFlag, setUnstakeProportionFlag] = useState(false)
+    const [unstakeProportionDone, setUnstakeProportionDone] = useState(false)
 
 
-    useEffect(() => {
-        console.log(account.stakeUnits)
-        // eslint-disable-next-line
-    }, [])
+    const { Option } = Select
 
     const getLink = (tx) => {
         return getEtherscanURL().concat('tx/').concat(tx)
     }
 
-    // const onUnitsChange = e => {
-    // 	setUnstakeUnits(convertToWei(e.target.value))
-    // }
+    const unstakeAsset = async () => {
+        const toVether = unstakeAssetSet.name === 'Vether'
+        setUnstakeAssetFlag(true)
+        const tx = await poolContract.methods.unstakeExactAsymmetric(
+            Web3.utils.toWei(unstakeAssetAmount.toString()),
+            ETH, toVether).send({ from: account.address })
+        setUnstakeAssetTx(tx.transactionHash)
+        setUnstakeAssetDone(true)
+    }
 
-    const unstake = async () => {
-        setBurnTknFlag(true)
-        const poolContract = new window.web3.eth.Contract(vetherPools2Abi(), vetherPools2Addr())
-        console.log(unstakeAmount, ETH)
-        const tx = await poolContract.methods.unstake(unstakeAmount, ETH).send({ from: account.address })
-        setTknTx(tx.transactionHash)
-        setLoaded2(true)
+    const unstakeProportion = async () => {
+        setUnstakeProportionFlag(true)
+        const tx = await poolContract.methods.unstake(
+            unstakeProportionAmount,
+            ETH).send({ from: account.address })
+        setUnstakeProportionTx(tx.transactionHash)
+        setUnstakeProportionDone(true)
+    }
+
+    const onUnstakeAssetChange = (value) => {
+        setUnstakeAssetSet(value)
+    }
+
+    const onAssetAmountChange = (value) => {
+        if (isNaN(value)) {
+            return
+        }
+        setUnstakeAssetAmount(value)
     }
 
     const onProportionAmountChange = (value) => {
         if (isNaN(value)) {
             return
         }
-        setUnstakeAmount(value)
+        setUnstakeProportionAmount(value)
     }
-
-    // const unstakeExact = async () => {
-    // 	setBurnTknFlag(true)
-    // 	const poolContract = new window.web3.eth.Contract(vetherPools2Abi(), vetherPools2Addr())
-    // 	console.log(unstakeUnits, ETH)
-    // 	const tx = await poolContract.methods.unstakeExact(unstakeUnits, ETH).send({ from: account.address })
-    // 	setTknTx(tx.transactionHash)
-    // 	setLoaded2(true)
-    // }
 
     return (
         <>
             <h2>REMOVE LIQUIDITY</h2>
-            <p>Remove your pooled assets.</p>
+            <h2 style={{ fontStyle: 'italic' }}>Choose asset to unstake.</h2>
             {(account.stakeUnits > 0) &&
             <>
+                <Row style={{ marginBottom: '2.66rem' }}>
+                    <Col span={24}>
+                        <LabelGrey display={'block'} style={{ marginBottom: '2.2rem', fontStyle: 'italic' }}>Choose an asset you wish to remove. Remove either just desired amount of single one or both assets at once in equal proportion.</LabelGrey>
+                        <Label display="block" style={{marginBottom: '0.55rem'}}>Asset Amount</Label>
+
+                        <Row>
+                            <Col xs={13} sm={13} lg={7}>
+                                <InputNumber
+                                    size="large"
+                                    className="forceHandleWrap"
+                                    min={0}
+                                    autoFocus="true"
+                                    onChange={onAssetAmountChange}
+                                    style={{ width: "calc(100% - 116px)", borderRight: 'none', borderRadius: '4px 0 0 4px' }}
+                                    step={0.1}
+                                    defaultValue="0"
+                                />
+                                <Select
+                                    size={'large'}
+                                    defaultValue={unstakeAssetSet.name}
+                                    className="borderLessLeft"
+                                    onChange={onUnstakeAssetChange}
+                                    style={{ minWidth: '83px' }}
+                                >
+                                    {assets.map((asset, index) => {
+                                        return(
+                                            <Option value={index} key={asset.name}>{asset.name}</Option>
+                                        )
+                                    })}
+                                </Select>
+                            </Col>
+
+                            <Col xs={11} sm={11} lg={17} style={{ marginTop: '-3px' }}>
+                                {unstakeAssetAmount > 0
+                                    ? <Button backgroundColor="transparent" onClick={unstakeAsset}>REMOVE >></Button>
+                                    : <Button backgroundColor="transparent" disabled>REMOVE >></Button>
+                                }
+                                <Sublabel margin={0}>REMOVE SINGLE ASSET</Sublabel>
+                                {unstakeAssetFlag &&
+                                    <>
+                                        {!unstakeAssetDone &&
+                                        <LoadingOutlined style={{ marginLeft: 20, fontSize: 15 }} />
+                                        }
+                                        {unstakeAssetDone &&
+                                        <>
+                                            <a href={getLink(unstakeAssetTx)} rel="noopener noreferrer" title="Transaction Link" target="_blank" style={{ color: "#D09800", fontSize: 12 }}>VIEW TRANSACTION -></a>
+                                        </>
+                                        }
+                                    </>
+                                }
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+
                 <Row>
-                    <Col xs={24} sm={16} xl={9}>
-                        <Label display="block" style={{marginBottom: '0.55rem'}}>Proportion</Label>
-                            <Col span={15} style={{ paddingLeft: 6, marginRight: 23 }}>
+                    <Col xs={24} sm={13} xl={7}>
+                        <Label display="block" style={{ marginBottom: '0.55rem' }}>Equal Proportion</Label>
+                            <Col span={13} style={{ paddingLeft: 6, marginRight: 23 }}>
                                 <Slider
                                     min={0}
                                     max={100}
                                     onChange={onProportionAmountChange}
-                                    value={typeof unstakeAmount === 'number' ? unstakeAmount : 0}
+                                    value={typeof unstakeProportionAmount === 'number' ? unstakeProportionAmount : 0}
                                     step={1}
                                 />
                             </Col>
-                            <Col span={3}>
+                            <Col span={2}>
                                 <InputNumber
                                     min={0}
                                     max={100}
+                                    size="large"
                                     onChange={onProportionAmountChange}
                                     style={{ marginBottom: 10 }}
                                     step={0.1}
                                     formatter={value => `${value}%`}
                                     parser={value => value.replace('%', '')}
                                     defaultValue="0"
-                                    value={unstakeAmount}
+                                    value={unstakeProportionAmount}
                                 />
                             </Col>
                     </Col>
-                    <Col xs={24} sm={7} style={{ paddingTop: 30 }}>
-                        {unstakeAmount > 0
-                            ? <Button backgroundColor="transparent" onClick={unstake}>REMOVE >></Button>
+                    <Col xs={24} sm={11} xl={17} style={{ paddingTop: 30 }}>
+                        {unstakeProportionAmount > 0
+                            ? <Button backgroundColor="transparent" onClick={unstakeProportion}>REMOVE >></Button>
                             : <Button backgroundColor="transparent" disabled>REMOVE >></Button>
                         }
-                        <Sublabel margin={0}>REMOVE LIQUIDITY FROM THE POOL</Sublabel>
-                        {burnTknFlag &&
+                        <Sublabel margin={0}>REMOVE PROPORTION</Sublabel>
+                        {unstakeProportionFlag &&
                         <>
-                            {!loaded2 &&
+                            {!unstakeProportionDone &&
                             <LoadingOutlined style={{ marginLeft: 20, fontSize: 15 }} />
                             }
-                            {loaded2 &&
+                            {unstakeProportionDone &&
                             <>
-                                <a href={getLink(tknTx)} rel="noopener noreferrer" title="Transaction Link" target="_blank" style={{ color: "#D09800", fontSize: 12 }}>VIEW TRANSACTION -></a>
+                                <a href={getLink(unstakeProportionTx)} rel="noopener noreferrer" title="Transaction Link" target="_blank" style={{ color: "#D09800", fontSize: 12 }}>VIEW TRANSACTION -></a>
                             </>
                             }
                         </>
@@ -494,7 +570,7 @@ export const UpgradeDialog = () => {
     }
 
     const loadAccountData = async (account) => {
-        const accountConnected = (await window.web3.eth.getAccounts())[0]
+        const accountConnected = (await window.web3.eth.getAccoueeeeents())[0]
         if (accountConnected) {
             const web3 = new Web3(new Web3.providers.HttpProvider(infuraAPI()))
             const contract = await new web3.eth.Contract(vetherAbi(), vetherAddr())
@@ -507,10 +583,7 @@ export const UpgradeDialog = () => {
             let poolData = await poolContract.methods.poolData(ETH).call()
             let poolShare = calcShare(getBN(stakeData.stakeUnits), getBN(poolData.poolUnits), getBN(poolData.asset), getBN(poolData.vether))
 
-            // let vetherShare = await poolContract.methods.getStakerShareVether(account, ETH).call()
-            // let assetShare = await poolContract.methods.getStakerShareAsset(account, ETH).call()
             let memberROI = await poolContract.methods.getMemberROI(account, ETH).call()
-            console.log(memberROI)
 
             const accountData = {
                 'address': account,
