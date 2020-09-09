65ed243f66f7f022656c387531ec090815579189
yellowgh0st <burim.berisa@protonmail.com>
2020-09-09 16:17:42 +0200

    Update account method

--- src/ui/pages/Stake.js
+++ src/ui/pages/Stake.js
@@ -64,9 +64,8 @@ const Stake = () => {
 	const connect = async () => {
 		const accountConnected = (await window.web3.eth.getAccounts())[0]
 		if(accountConnected) {
-			window.web3 = new Web3(window.ethereum)
-			var accounts = await window.web3.eth.getAccounts()
-			const address = await accounts[0]
+			const accounts = await window.web3.eth.getAccounts()
+			const address = accounts[0]
 			await loadAccountData(address)
 			setConnected(true)
 		}
