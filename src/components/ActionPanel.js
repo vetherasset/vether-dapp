173675330894f21e0083d6c2fefde325221c063b
Augustin Berisa <augustin.be@protonmail.com>
2021-03-31 21:11:30 +0200

    Reduce action panel max width

--- src/components/ActionPanel.js
+++ src/components/ActionPanel.js
@@ -47,7 +47,7 @@ export const ActionPanel = (props) => {
 			color='black'
 			pos={isOpen > -1 ? 'fixed' : { base: 'fixed', sm: 'relative' }}
 			bottom={isOpen > -1 ? { base: '0', md: '35px' } : '0'}
-			maxWidth={defaults.layout.width}
+			maxWidth={'60ch'}
 			borderRadius={{ base: isOpen > -1 ? '0.8rem 0.8rem 0 0' : '0', sm: '0.8rem' }}
 			left='50%'
 			transform='translateX(-50%)'
