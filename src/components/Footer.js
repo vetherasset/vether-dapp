28bb6b48a86190e7c14fc936deddf4dbf2102669
Augustin Berisa <burim.berisa@protonmail.com>
2021-04-26 04:59:19 +0200

    Add analytics into footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Flex, Link, useBreakpointValue, Image } from '@chakra-ui/react'
 import { FaDiscord, FaTelegramPlane, FaGithub } from 'react-icons/fa'
 import { IoIosPaper } from 'react-icons/io'
+import { IoBarChart } from 'react-icons/io5'
 import { MdHelp } from 'react-icons/md'
 import UniswapIcon from '../assets/svg/uniswap.svg'
 
@@ -31,7 +32,7 @@ const Links = (props) => {
 	}
 
 	return(
-		<Flex {...props} w='100%' mb='15px' justifyContent='space-between' flexWrap='wrap'>
+		<Flex {...props} w='100%' mb='75px' justifyContent='space-between' flexWrap='wrap'>
 			<Link {...linkStyle} isExternal href='https://app.uniswap.org/#/swap/0x4ba6ddd7b89ed838fed25d208d4f644106e34279'>
 				<Image src={UniswapIcon} display='inline-block' marginInlineEnd='0.5rem' width='14.4px' verticalAlign='baseline' />
 				Uniswap
@@ -39,6 +40,10 @@ const Links = (props) => {
 			<Link {...linkStyle} isExternal href='https://wp.vetherasset.io/'>
 				<IoIosPaper style={iconStyle}/>
 				Whitepaper
+			</Link>
+			<Link {...linkStyle} isExternal href='https://stats.vetherasset.io/'>
+				<IoBarChart style={iconStyle}/>
+				Analytics
 			</Link>
 			<Link {...linkStyle} isExternal href='https://docs.vetherasset.io/'>
 				<MdHelp style={iconStyle}/>
