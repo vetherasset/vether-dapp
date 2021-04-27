eef4db00ecbb2d9f6f89894b74de54f0e5a93689
Augustin Berisa <burim.berisa@protonmail.com>
2021-04-27 12:53:32 +0200

    Increase poll times

--- src/common/defaults.js
+++ src/common/defaults.js
@@ -16,7 +16,7 @@ defaults.network.address.uniswap.veth = '0x3696Fa5Ad6e5C74fdcBCEd9aF74379D94C4B7
 defaults.network.address.uniswap.usdc = '0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc'
 
 defaults.poll = {}
-defaults.poll.time = 60000
+defaults.poll.time = 180000
 
 defaults.layout = {}
 defaults.layout.width = '768px'
