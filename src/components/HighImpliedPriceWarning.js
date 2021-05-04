92fe8200e629c085fab7a4f7a877b42ba335bd83
Augustin Berisa <burim.berisa@protonmail.com>
2021-05-04 05:49:17 +0200

    Don not break line on mobile

--- src/components/HighImpliedPriceWarning.js
+++ src/components/HighImpliedPriceWarning.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {
 	AlertIcon, Box, Alert, AlertTitle, AlertDescription,
-	Link, CloseButton, ScaleFade,
+	Link, CloseButton, ScaleFade, useBreakpointValue,
 } from '@chakra-ui/react'
 
 export const HighImpliedPriceWarning = (props) => {
@@ -34,7 +34,10 @@ export const HighImpliedPriceWarning = (props) => {
 					<AlertTitle>Are you sure you want to burn?</AlertTitle>
 					<AlertDescription display='block'>
 					Implied price {props.impliedValue ? <i>({props.impliedValue})</i> : ''} is already above market{props.price ? <i> ({props.price})</i> : ''}!
-						<br/>
+						{useBreakpointValue({
+							base: ' ',
+							sm: <br/>,
+						})}
 					Consider <Link isExternal href='https://app.uniswap.org/#/swap/0x4ba6ddd7b89ed838fed25d208d4f644106e34279' style={{ textDecoration: 'underline' }}>buying on Uniswap</Link> now instead.
 					</AlertDescription>
 				</Box>
